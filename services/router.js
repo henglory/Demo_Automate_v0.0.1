a0f7a27e892c617b0e9d93c6bafd6e3c584ebf1a
Nuttapong <nuttapong@Nuttapongs-MacBook-Pro.local>
2019-07-09 14:55:54 +0700

    add router

--- services/router.js
+++ services/router.js
@@ -1,6 +1,8 @@
 import express from 'express'
-
+import { healthcheckTestsuiteRegister, healthcheckTestsuitHtmlRegister } from './healthcheck_testsuit'
 var router = express.Router()
 
+healthcheckTestsuiteRegister(router)
+healthcheckTestsuitHtmlRegister(router)
 
 export { router }
